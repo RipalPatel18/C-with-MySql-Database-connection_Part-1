8557c45cea5999eb937fcbe7cd8972168bde5b9c
RipalPatel18 <n01578468@humber.ca>
2025-08-24 12:26:49 -0400

    Initial commit

---
+++ blogdb/Controllers/ArticleAPIController.cs
@@ -0,0 +1,56 @@
+﻿using blogdb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+
+namespace blogdb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleAPIController : ControllerBase
+    {
+        //access to the blog object
+        private BlogDbContext Blog = new BlogDbContext();
+
+        [HttpGet(template: "ListArticleNames")]
+        public string ListArticleNames()
+        {
+            //create empty list of strings
+            List<string> ArticleTitles = new List<string>();
+
+            //open the connection to the database
+            MySqlConnection connection = Blog.AccessDatabase();
+
+            //open connection
+            connection.Open();
+
+            //Sql query to select all articles
+            string query = "SELECT * from articles";
+
+            //Create Command
+            MySqlCommand command = connection.CreateCommand();
+
+            //create a command to execute the query
+            command.CommandText = query;
+
+            // Execute the command and retrieve the result set
+            MySqlDataReader resultSet = command.ExecuteReader();
+
+            // Process the result set
+            while (resultSet.Read())
+            {
+                String ArticleTitle = resultSet["articletitle"].ToString();
+                ArticleTitles.Add(ArticleTitle);
+                //another way 
+                //ArticleTitles.Add(resultSet.GetString("title")); // Assuming "title" is the column name
+            }
+
+            resultSet.Close();
+            connection.Close();
+
+            // Return the list of article titles as a comma-separated string
+            return string.Join(", ", ArticleTitles);
+        }
+    }
+}
---
+++ blogdb/Models/BlogDbContext.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+
+
+namespace blogdb.Models
+{
+    public class BlogDbContext
+    {
+        private static string User { get { return "root"; } }
+
+        private static string Password { get { return " "; } }
+
+        private static string Database { get { return "blog"; } }
+
+        private static string Server { get { return "localhost"; } }
+
+        private static string Port { get { return "3306"; } }
+
+
+        protected static string ConnectionString
+        {
+            get
+            {
+                return $"Server={Server};" +
+                    $"Port={Port};" +
+                    $"Database={Database};" +
+                    $"User ID={User};" +
+                    $"Password={Password};";
+            }
+        }
+        public MySqlConnection AccessDatabase()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
